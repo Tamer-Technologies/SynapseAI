ddc6124504e9a103e76168406e12c7e12639b50d
Tamer Technologies <216339134+Tamer-Technologies@users.noreply.github.com>
2025-11-25 23:04:40 +0200

    feat: create Hero section component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,12 @@ import { Link } from "react-router-dom";
 import Robot from "../assets/images/robot.png";
 import RobotShadow from "../assets/images/robot-shadow.png";
 import Sparks from "./units/Sparks";
+import useMouseParallex from "../hooks/useMouseParallex";
+import Notification from "./units/Notification";
 
 const Hero = () => {
+  const ref = useMouseParallex();
+
   return (
     <>
       <Section
@@ -36,15 +40,25 @@ const Hero = () => {
           <div className="max-w-4xl gradient-border aspect-[3/1.5] rounded-2xl">
             <img src={Robot} alt="robot" className="rounded-xl" />
           </div>
+          <Notification
+            label="Code Generation"
+            date="1m ago"
+            className="top-[10%] left-[-10%]"
+          />
+          <Notification
+            label="Code Generation"
+            date="10m ago"
+            className="top-[60%] right-[-10%] absolute"
+          />
           <img
             src={RobotShadow}
             aria-hidden
             className="absolute  h-auto w-full left-0 -top-[47.5%] -z-10 opacity-50"
           />
         </div>
-        <div className="absolute w-[60%] aspect-3/2 bg-primary-color1 blur-[9999px] opacity-25 rounded-full -z-10 pointer-events-none"></div>
+        <div className="absolute  w-[60%] aspect-3/2 bg-primary-color1 blur-[9999px] opacity-25 rounded-full -z-10 pointer-events-none"></div>
 
-        <Sparks className="-z-20" />
+        <Sparks className="-z-20" ref={ref} />
       </Section>
     </>
   );
